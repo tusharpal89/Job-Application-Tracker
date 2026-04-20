231c7b34500502149d000324e3cdac198cd35857
Tushar Pal <tusharshiraswal@gmail.com>
2026-04-20 19:59:08 +0530

    fix: updated cacheComponents config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
+ 
   cacheComponents: true,
-}
+
+
 };
 
 export default nextConfig;
